ba29722e4d79ee8831d044c3b85735ef826317c9
Anderson Viana <valladau2@gmail.com>
2021-08-19 16:13:23 -0300

    Initial Commit

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.listen(
+    port,
+    () => console.log(`A API está funionando na porta ${port}`)
+)
+
+app.get('/teste', (req, res) => res
+    .send({ Mensagem: 'teste' })
+    .status(200)
+)
+
+module.exports = app
